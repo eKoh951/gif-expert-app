7464790b4666742a3d5553c7aa564ff920b478cc
eKoh95 <greatekoh@gmail.com>
2020-08-11 23:06:04 -0600

    Docs agregados

---
+++ docs/precache-manifest.7591db23437b72b893ec47b25588bb89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a331ec2ac5cdf54745a59886cb0eec22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ba2dd1fdd3f9836efd",
+    "url": "/static/css/main.91605e26.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ba2dd1fdd3f9836efd",
+    "url": "/static/js/main.78c37b8e.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
