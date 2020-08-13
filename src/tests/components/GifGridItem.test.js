3bc67ef194d1001ce0f9d6c5f6e45f81ef0fb15f
eKoh95 <greatekoh@gmail.com>
2020-08-13 13:31:46 -0600

    Added properties and class tests to GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,14 +5,13 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Prueba de componente GifGridItem', () => {
 	let wrapper;
+	const img = {
+		id: 'mAJxVnLmZcZMzhyPvO',
+		title: 'One Punch Man GIF by memecandy',
+		url: 'https://media1.giphy.com/media/mAJxVnLmZcZMzhyPvO/giphy.gif?cid=91b2ca0cphho7fz2liinkjwa9hugoqx2l3ka56bmxa42q50v&rid=giphy.gif'
+	}
 
 	beforeAll(() => {
-		const img = {
-			id: 'mAJxVnLmZcZMzhyPvO',
-			title: 'One Punch Man GIF by memecandy',
-			url: 'https://media1.giphy.com/media/mAJxVnLmZcZMzhyPvO/giphy.gif?cid=91b2ca0cphho7fz2liinkjwa9hugoqx2l3ka56bmxa42q50v&rid=giphy.gif'
-		}
-		
 		wrapper = shallow(
 		<GifGridItem
 			key={ img.id }
@@ -23,4 +22,23 @@ describe('Prueba de componente GifGridItem', () => {
 	test('debe de mostrar el componente correctamente', () => {
 		expect(wrapper).toMatchSnapshot();
 	})
+
+	test('debe de tener un parrafo con el title', () => {
+		const p = wrapper.find('p');
+		expect( p.text().trim() ).toBe( img.title );
+	})
+
+	test('debe de tener una imagen con src y alt de los props', () => {
+		const image = wrapper.find('img');
+		// console.log( image.props() );
+		expect( image.prop('src') ).toBe( img.url );
+		expect( image.prop('alt') ).toBe( img.title );
+	})
+	
+	test('debe de tener animate__fadeIn', () => {
+		const div = wrapper.find('div');
+		expect( div.prop('className') ).toContain( 'animate__fadeIn' );
+	})
+	
+	
 })
